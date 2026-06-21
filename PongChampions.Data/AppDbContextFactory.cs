0520047d4c54ed2db19def6dd86797113475ccbc
mhrastegari <mh.rastegari@outlook.com>
2026-06-21 23:17:37 +0330

    add AppDbContextFactory

---
+++ PongChampions.Data/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PongChampions.Data.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        var connectionString =
+            $"Host=localhost;" +
+            $"Port=5432;" +
+            $"Database=pongchampions;" +
+            $"Username={Environment.GetEnvironmentVariable("POSTGRES_USER")};" +
+            $"Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")}";
+
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
